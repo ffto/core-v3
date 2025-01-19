ad50824d6afead134c2a92f2f837d073461b4f4b
Maxime Lefrancois <maxlefrancois@gmail.com>
2025-01-18 20:56:50 -0500

    no message

---
+++ theme/routes/class.js
@@ -0,0 +1,40 @@
+Component(function Camera (){
+	this.$$extend = '';
+
+	this.$$       = {};
+	this.$$style  = {};
+	this.$$states = {};
+
+	this.$el   = Query();
+	this.$win  = Query();
+	this.$body = Query();
+	this.$head = Query();
+
+	// init --------------------------------------------------------------------
+	this.data = {
+		
+	};
+	
+	this.init = function (){
+		// this._super();	
+	};
+
+	// private -----------------------------------------------------------------
+	this._private = function (){
+
+	};
+
+	// events ------------------------------------------------------------------
+	this.onPageResize = function (e){
+
+	};
+
+	// method ------------------------------------------------------------------
+	this.open = function (){
+
+	};
+
+	this.close = function (){
+		
+	};	
+});
